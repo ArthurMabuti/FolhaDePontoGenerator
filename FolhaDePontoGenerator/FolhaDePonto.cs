0d62296d790224725b4e5dfc11a45f40c153e60b
pereira.arthur.ext <pereira.arthur.ext@br.panasonic.com>
2023-10-27 16:18:39 -0300

    Add project files.

---
+++ FolhaDePontoGenerator/FolhaDePonto.cs
@@ -0,0 +1,34 @@
+﻿internal partial class Program
+{
+    public class FolhaDePonto
+    {
+        public DateTime Data { get; set; }
+        public string? DiaDaSemana => Data.ToString("dddd");
+        public DateTime Entrada { get; set; }
+        public DateTime InicioAlmoco { get; set; }
+        public DateTime FimAlmoco { get; set; }
+        public DateTime Saida { get; set; }
+        public string? HelpDesk { get; set; }
+        public int Quilometragem { get; set; }
+        public double Pedagio { get; set; }
+
+        public List<DateTime> GerarDatas(DateTime dataInicial)
+        {
+            DateTime novaData = CorrigirAno(dataInicial);
+            List<DateTime> datas = new();
+            DateTime dataFinal = novaData.AddMonths(1);
+            while (novaData != dataFinal)
+            {
+                datas.Add(novaData);
+                novaData = novaData.AddDays(1);
+            }
+            return datas;
+        }
+
+        private DateTime CorrigirAno(DateTime dataInicial)
+        {
+            DateTime novaData = DateTime.Parse($"{dataInicial.Day}/{dataInicial.Month}/{DateTime.Now.Year}");
+            return novaData;
+        }
+    }
+}
---
+++ FolhaDePontoGenerator/Program.cs
@@ -0,0 +1,18 @@
+﻿using OfficeOpenXml;
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        FileInfo file = new(@"C:\Users\Desktop\Excel.xlsx");
+
+        //DateTime dataInicial = DateTime.Parse("25/out");
+        //var datas = GerarDatas(dataInicial);
+
+        //foreach(DateTime data in datas)
+        //{
+        //    Console.WriteLine(data.ToString("dd/MMM/yyyy"));
+        //}
+    }
+}
